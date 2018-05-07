330101a174ba5709cbe68d1f828860b3217bdaf4
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-07 09:10:03 +0200

    Improve 2 functionsin  script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
 $(function() {
-/*
-  var supportedFlag = $.keyframe.isSupported();
-  $.keyframe.debug = true;
-
- $.keyframe.define([{
-    name: 'card-animation',
-    '0%': {
-        'background-color': '#D7BCEC',
-        'color' : '#5F2900',
-        'transform': 'matrix(1, 0, 0, 1, 0, 0 )'
-    },
-    '25%': {
-        'background-color': '#ac74d8',
-        'color' : '#cc5800',
-        'transform': 'matrix(1.25, 0, 0.25, 1.25, 0, 0 )'
-    },
-    '50%': {
-        'background-color': '#ac74d8',
-        'color' : '#ffb780',
-        'transform': 'matrix(1.5, 0, 0.5, 1.5, 0, 0 )'
-    },
-    '75%': {
-        'background-color': '#ac74d8',
-        'color' : '#cc5800',
-        'transform': 'matrix(1.25, 0, 0.25, 1.25, 0, 0 )'
-    },
-    '100%': {
-        'background-color': '#D7BCEC',
-        'color' : '#5F2900',
-        'transform': 'matrix(1, 0, 0, 1, 0, 0 )'
-    }
- }]);*/
 
   function randomString() {
     var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
@@ -44,23 +12,9 @@ $(function() {
   function initSortable() {
     var self = this;
     $('.column-card-list').sortable({
-      delay: 150,
-      opacity: 0.4,
       connectWith: '.column-card-list',
-      placeholder: 'card-placeholder',
-        /*playKeyframe({
-          name: "card-animation",
-          duration: "1s",
-          timingFunction: 'ease',
-          direction: 'normal',
-          fillMode: 'forwards',
-          complete: increment
-        });*/
-
+      placeholder: 'card-placeholder'
     }).disableSelection();
-    $('.column-card-list .card').click(function() {
-      $('.column-card-list .card').addClass('card-animated');
-      });
   }
 
   function Column(name) {
@@ -83,11 +37,18 @@ $(function() {
             self.removeColumn();
       });
       //Add a note after clicking on the button:
+
           $columnAddCard.click(function() {
-            self.addCard(new Card(prompt("Enter the name of the card.")));
-          });
+              var name = prompt("Enter the name of the card.");
+                if (name == null || name == "") {
+                  alert("User does not want to add a card.")
+                } else {
+                   self.addCard(new Card(name));
+                }
+            });
 
       // CONSTRUCTION COLUMN ELEMENT
+
       $column.append($columnTitle)
               .append($columnDelete)
               .append($columnAddCard)
@@ -114,17 +75,21 @@ $(function() {
     this.$element = createCard();
 
     function createCard() {
+
       // CREATING THE BLOCKS
+
       var $card = $('<li>').addClass('card');
       var $cardDescription = $('<p>').addClass('card-description').text(self.description);
       var $cardDelete = $('<button>').addClass('card-btn-delete').text('x');
 
       // BINDING TO CLICK EVENT
+
       $cardDelete.click(function(){
             self.removeCard();
       });
 
       // COMBINING BLOCKS AND RETURNING THE CARD
+
       $card.append($cardDelete)
           .append($cardDescription);
 
@@ -151,26 +116,36 @@ $(function() {
   $('.create-column')
     .click(function() {
       var name = prompt('Enter a column name');
-      var column = new Column(name);
-          board.addColumn(column);
+      if (name == null || name =="") {
+        alert("User does not want to creat a column.");
+      } else {
+        var column = new Column(name);
+            board.addColumn(column);
+      }
+
     });
 
   // CREATING COLUMNS
+
   var todoColumn = new Column('To do');
   var doingColumn = new Column('Doing');
   var doneColumn = new Column('Done');
+
   // GETTING THE PLACEHOLDER OPITON
 
   // ADDING COLUMNS TO THE BOARD
+
   board.addColumn(todoColumn);
   board.addColumn(doingColumn);
   board.addColumn(doneColumn);
 
   // CREATING CARDS
+
   var card1 = new Card('New task');
   var card2 = new Card('Create kanban boards');
 
   //ADDING CARDS TO COLUMNS
+
   todoColumn.addCard(card1);
   doingColumn.addCard(card2);
 });
